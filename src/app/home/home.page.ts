2b227e2f5167f970fbfc7aea6cfe6f4e1f4f2589
anghel3m <adms95@outlook.es>
2025-07-14 18:20:07 -0500

    storage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
+import { StorageService } from "../services/storage.service";
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -10,7 +10,7 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
   imports: [IonicModule, CommonModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class HomePage {
+export class HomePage implements OnInit {
   //temas
   temaClaro = 'var( --tema-claro-fondo)';
   temaOscuro = 'var( --tema-oscuro-fondo)';
@@ -22,31 +22,38 @@ export class HomePage {
 
   generes = [
     {
-    title: "musica clasica",
-    image: "/../../assets/images/musica-clasica.png",
-    description: "La música clásica es una excelente compañía para estudiar, ya que su estructura armónica y ritmos suaves favorecen la concentración. Al no tener letras, evita distracciones y ayuda a mantener la mente enfocada en las tareas académicas. Estudios demuestran que piezas de Mozart o Bach estimulan la memoria y la retención de información."
-  },
-   {
-    title: "Deep House",
-    image: "/../../assets/images/musica-deephouse.png",
-    description: "La música deep house, con sus ritmos envolventes y melodías repetitivas, aumenta la energía y la concentración al trabajar. Su tempo constante favorece la productividad, manteniendo un flujo de trabajo constante sin distracciones. Además, el bajo profundo y los sonidos atmosféricos crean un ambiente dinámico que combate el agotamiento."
-  },
+      title: "musica clasica",
+      image: "/../../assets/images/musica-clasica.png",
+      description: "La música clásica es una excelente compañía para estudiar, ya que su estructura armónica y ritmos suaves favorecen la concentración. Al no tener letras, evita distracciones y ayuda a mantener la mente enfocada en las tareas académicas. Estudios demuestran que piezas de Mozart o Bach estimulan la memoria y la retención de información."
+    },
     {
-    title: "Vallenato",
-    image: "/../../assets/images/musica-vallenato.png",
-    description: "El vallenato, con sus letras emotivas y ritmo alegre, anima el ambiente laboral. Su melodía de acordeón y caja motiva sin distraer, ideal para trabajos creativos o en equipo. Además, su esencia folclórica genera un ambiente cálido y energizante. Perfecto para mantener el ánimo alto durante la jornada."
-  },
-      {
-    title: "Reggaetón",
-    image: "/../../assets/images/musica-reggeton.png",
-    description: "El reggaetón, con su ritmo contagioso y beats energéticos, acelera el ánimo y la productividad en trabajos dinámicos. Su flow repetitivo ayuda a mantener el ritmo en tareas repetitivas o físicas, mientras que las letras motivadoras (o el instrumental) pueden aumentar la energía. "
-  }  
- ]
-  constructor() { }
+      title: "Deep House",
+      image: "/../../assets/images/musica-deephouse.png",
+      description: "La música deep house, con sus ritmos envolventes y melodías repetitivas, aumenta la energía y la concentración al trabajar. Su tempo constante favorece la productividad, manteniendo un flujo de trabajo constante sin distracciones. Además, el bajo profundo y los sonidos atmosféricos crean un ambiente dinámico que combate el agotamiento."
+    },
+    {
+      title: "Vallenato",
+      image: "/../../assets/images/musica-vallenato.png",
+      description: "El vallenato, con sus letras emotivas y ritmo alegre, anima el ambiente laboral. Su melodía de acordeón y caja motiva sin distraer, ideal para trabajos creativos o en equipo. Además, su esencia folclórica genera un ambiente cálido y energizante. Perfecto para mantener el ánimo alto durante la jornada."
+    },
+    {
+      title: "Reggaetón",
+      image: "/../../assets/images/musica-reggeton.png",
+      description: "El reggaetón, con su ritmo contagioso y beats energéticos, acelera el ánimo y la productividad en trabajos dinámicos. Su flow repetitivo ayuda a mantener el ritmo en tareas repetitivas o físicas, mientras que las letras motivadoras (o el instrumental) pueden aumentar la energía. "
+    }
+  ]
+  constructor(private storageService: StorageService) { }
 
-cambiarTema(){
-//if ternario
-this. temaActual = this.temaActual === this.temaOscuro ? this.temaClaro : this.temaOscuro
-this. tematextoActual = this.tematextoActual === this.temaOscuroTexto ? this.temaClaroTexto : this.temaOscuroTexto
-}
+  async ngOnInit() {
+    
+  }
+
+  async cambiarTema() {
+    //if ternario
+    this.temaActual = this.temaActual === this.temaOscuro ? this.temaClaro : this.temaOscuro
+    this.tematextoActual = this.tematextoActual === this.temaOscuroTexto ? this.temaClaroTexto : this.temaOscuroTexto
+    await this.storageService.set('theme', this.temaActual)
+    console.log('tema guardado: ', this.temaActual)
+
+  }
 }
