738af5a051eec6de00002ea3273627ab76a7e3b0
anghel3m <adms95@outlook.es>
2025-07-23 19:03:19 -0500

    menu funcionando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,10 @@ import { IntroGuard } from './guards/intro.guard';
 import { loginGuard } from './guards/login.guard';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage), canActivate: [IntroGuard,loginGuard]
-  },
+
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'menu/home',
     pathMatch: 'full',
   },
   {
@@ -23,5 +20,16 @@ export const routes: Routes = [
   {
     path: 'signup',
     loadComponent: () => import('./signup/signup.page').then( m => m.SignupPage)
+  },
+  {
+    path: 'menu',
+    loadComponent: () => import('./menu/menu.page').then( m => m.MenuPage),
+    children: [
+        {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage), canActivate: [IntroGuard,loginGuard]
+  },
+
+    ]
   },
 ];
